be2542c985a9863cb91d8160c07d0e2a16efa6a8
Al Fi <notifyalfi+gitlab@pm.me>
2023-11-29 23:47:48 +0800

    Completed exercise-1

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -1,10 +1,8 @@
 package org.advent.day1;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestElfShould {
@@ -16,21 +14,35 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addCalories(1000);
+        assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        elf1.addCalories(1000);
+        Elf elf2 = new Elf();
+        elf2.addCalories(500);
+        assertThat(elf1.compareTo(elf2), greaterThan(0));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        elf1.addCalories(500);
+        Elf elf2 = new Elf();
+        elf2.addCalories(500);
+        assertThat(elf1.compareTo(elf2), equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        elf1.addCalories(100);
+        Elf elf2 = new Elf();
+        elf2.addCalories(500);
+        assertThat(elf1.compareTo(elf2), lessThan(0));
     }
 }
